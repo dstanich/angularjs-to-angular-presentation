b329fd018f2d86f6f5b9a2dae31965c7fb740ce7
David Stanich <dstanich@gmail.com>
2018-08-08 00:33:52 -0500

    updates to presentation

--- onpush-example/src/app/app.component.ts
+++ onpush-example/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Item } from './item/item.component';
 
 @Component({
@@ -6,7 +6,7 @@ import { Item } from './item/item.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   items: Array<Item> = [];
 
